94843c665d0c2a289b28a829c26ecfb5b2a93d7c
Ian Jennings <ian@meetjennings.com>
2017-08-24 21:06:43 -0500

    update examples to look in node_modules rather than chat-engine local repo

--- react-native/App.js
+++ react-native/App.js
@@ -7,9 +7,10 @@ const now = new Date().getTime();
 const username = ['user', now].join('-');
 
 const ChatEngine = ChatEngineCore.create({
-  publishKey: 'pub-c-ea1b85f7-8895-4514-b0e0-b505eaaa1b62',
-  subscribeKey: 'sub-c-7397fa12-43a3-11e6-bfbb-02ee2ddab7fe'
+    publishKey: 'pub-c-bcf4e625-d5e0-45de-9f74-f222bf63a4a1',
+    subscribeKey: 'sub-c-70f29a7c-8927-11e7-af73-96e8309537a2',
 }, {
+  globalChannel: 'chat-engine-react-native',
   insecure: true
 });
 
